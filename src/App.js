dc8cefe420e5650b3210a3847706655e1657c389
ESMERALDOJr <esmeraldo.log@hotmail.com>
2021-10-03 14:07:37 -0300

    Calculadora em aperfeiçoamento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculadora extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            a: '',
+            b: '',
+            result: ''
+        };
+        this.handleChangeA = this.handleChangeA.bind(this);
+        this.handleChangeB = this.handleChangeB.bind(this);
+        this.handleCalculate = this.handleCalculate.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <fieldset>
+                        <div>
+                            <label for="valor1">Número 1: </label>
+                            <input type="text" onChange={this.handleChangeA} value={this.state.a} />
+                        </div>
+                        <div>
+                            <label for="valor1">Número 2: </label>
+                            <input type="text" onChange={this.handleChangeB} value={this.state.b} />
+                        </div>
+
+                        <label for="calcular">Escolha a operação desejada:</label><br></br>
+                        <button type="button" onClick={this.handleCalculate}>Adição</button>
+                        <button type="button" onClick={this.handleCalculate}>Subtração</button>
+                        <button type="button" onClick={this.handleCalculate}>Multiplicação</button>
+                        <button type="button" onClick={this.handleCalculate}>Divisão</button><br></br>
+<br></br>
+                        <div>
+                            <label for="result">Resultado: </label>
+                            <input type="text" value={this.state.result} disabled="disabled" />
+                        </div>
+
+                    </fieldset>
+                </form>
+            </div >
+        );
+    }
+
+    handleChangeA(e) {
+        this.setState({ a: e.target.value });
+    }
+
+    handleChangeB(e) {
+        this.setState({ b: e.target.value });
+    }
+
+    handleCalculate(e) {
+        e.preventDefault();
+
+        switch (e.target.innerText) {
+            case 'Adição':
+                var result = parseFloat(this.state.a) + parseFloat(this.state.b);
+                break;
+            case 'Subtração':
+                var result = parseFloat(this.state.a) - parseFloat(this.state.b);
+                break;
+            case 'Multiplicação':
+                var result = parseFloat(this.state.a) * parseFloat(this.state.b);
+                break;
+            case 'Divisão':
+                var result = parseFloat(this.state.a) / parseFloat(this.state.b);
+                break;
+        }
+
+        this.setState({ result: '' + result });
+    }
+
 }
+export default Calculadora;
 
-export default App;
+
+
+
